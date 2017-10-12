21eb276f390b9030e505410137948e1a7daa37c3
Asus Home <Asus Home@DESKTOP-CGDSSVT>
2017-10-12 23:28:35 +0300

    12/10/2017 Stefan
    Creare clase ConexiuneFactory.php, UserView.php, Log4Debug.php

--- web_resources/web_testing/paginaTestareMetode.php
+++ web_resources/web_testing/paginaTestareMetode.php
@@ -2,9 +2,56 @@
 <head></head>
 <body>
 
-Testare php : <?php echo'test';?>
-<br>
-Testare conexiune db: <?php echo'test';?>
+<!-- Report all errors except E_NOTICE -->
+<!-- This is the default value set in php.ini -->
 
+Testare configInfoDB.ini luare de valori din proprietati:<br>
+<?php 
+
+#define('HOME_DIR', 'C:\xampp\htdocs\project\e-commerce\');
+
+
+// error_reporting(E_ALL ^ E_NOTICE);
+// $ini_array= parse_ini_file("../../DB_development/mySQL/configInfoDB.ini");
+// if ($ini_array) {
+// 	echo 'Loaded configInfoDB.ini: ';
+// } else {
+// 	echo 'A php.ini file is not loaded';
+// }
+// print_r($ini_array);
+
+// echo "<br>";
+// echo $ini_array[DbUserName]."<br>";
+// echo $ini_array[DbUserPass]."<br>";
+// echo $ini_array[DbSchemaName]."<br>";
+// echo $ini_array[DbIP]."<br>";
+// echo $ini_array[DbPort]."<br>";
+
+
+
+
+
+
+// $files1 = scandir('.');
+// print_r($files1);
+// $files2 = scandir('./..');
+// print_r($files2);
+// $files3 = scandir('./../../');
+// print_r($files3);
+// $files3 = scandir('./../../BE_development/PHP/dbConnectorClasses/');
+// print_r($files3);
+
+
+
+
+
+
+include "./../../BE_development/PHP/dbConnectorClasses/ConexiuneFactory.php";
+
+
+$conn= (new ConexiuneFactory())->getConexiune();
+
+
+?>
 </body>
 </html>
