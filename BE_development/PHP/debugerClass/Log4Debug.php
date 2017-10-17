3911b11d2455f561a1e60d294a98c62d8aaf182c
Asus Home <Asus Home@DESKTOP-CGDSSVT>
2017-10-17 19:21:07 +0300

    Creare de noi clase DbHandler si Log4Debug

--- BE_development/PHP/debugerClass/Log4Debug.php
+++ BE_development/PHP/debugerClass/Log4Debug.php
@@ -1,12 +1,12 @@
 <?php 
 # Pentru anularea de PHP Notice:
 error_reporting(E_ALL ^ E_NOTICE);
+include 'ILog4Debug.php';
 
-// Exemplu
-// $log4Debug =  new Log4Debug();
-// $log4Debug->debug_StringValue("test ",123);
-class Log4Debug{
-
+class Log4Debug implements ILog4Debug{
+	const headerDebug = "DEBUG"."::";
+	const headerAlert = "ALERT"."::";
+	
 	protected $msgErr;
 	
 	public function __construct(){
@@ -15,38 +15,34 @@ class Log4Debug{
 	}
 	
 	public static function debug_String($string){
-		$msgErr = "DEBUG"."::".(new DateTime())->format('Y-m-d H:i:s')."::- ".$string;
+		$msgErr = self::headerDebug.(new DateTime())->format('Y-m-d H:i:s')."::- ".$string;
 		file_put_contents('../../../logFile.txt',$msgErr.PHP_EOL, FILE_APPEND);
 	}
 	
 	public static function debug_StringValue($string,$value){
-		$msgErr ="DEBUG"."::".(new DateTime())->format('Y-m-d H:i:s')."::- ".$string.$value;
+		$msgErr = self::headerDebug.(new DateTime())->format('Y-m-d H:i:s')."::- ".$string.$value;
 		file_put_contents('../../../logFile.txt',$msgErr.PHP_EOL, FILE_APPEND);
 	}
 	#---------------------------------------------------------------------------------------------------------------------
 	
 	public static function alert_String($string){
-		$msgErr = "ALERT"."::".(new DateTime())->format('Y-m-d H:i:s')."::- ".$string;
+		$msgErr = self::headerAlert.(new DateTime())->format('Y-m-d H:i:s')."::- ".$string;
 		file_put_contents('../../../logFile.txt',$msgErr.PHP_EOL, FILE_APPEND);
 	}
 	public static function alert_StringValue($string,$value){
-		$msgErr = "ALERT"."::".(new DateTime())->format('Y-m-d H:i:s')."::- ".$string.$value;
+		$msgErr = self::headerAlert.(new DateTime())->format('Y-m-d H:i:s')."::- ".$string.$value;
 		file_put_contents('../../../logFile.txt',$msgErr.PHP_EOL, FILE_APPEND);
 	}
 	#---------------------------------------------------------------------------------------------------------------------
-	/*Associative Array
-		To loop through and print all the values of an associative array, you could use a foreach loop, like this:
-		Ex: $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");   <-- associative array
-	*/
 	public static function alert_AssArray($array){
 		foreach($array as $x => $x_value) {
-			$msgErr = "ALERT"."::".(new DateTime())->format('Y-m-d H:i:s')."::- "."ARRAY $array: (KEY)$x - (VALUE)$x_value";
+			$msgErr = self::headerAlert.(new DateTime())->format('Y-m-d H:i:s')."::- "."ARRAY $array: (KEY)$x - (VALUE)$x_value";
 			file_put_contents('../../../logFile.txt',$msgErr.PHP_EOL, FILE_APPEND);
 		}
 	}
 	public static function debug_AssArray($array){
 		foreach($array as $x => $x_value) {
-			$msgErr = "DEBUG"."::".(new DateTime())->format('Y-m-d H:i:s')."::- "."ARRAY $array: (KEY)$x - (VALUE)$x_value";
+			$msgErr = self::headerDebug.(new DateTime())->format('Y-m-d H:i:s')."::- "."ARRAY $array: (KEY)$x - (VALUE)$x_value";
 			file_put_contents('../../../logFile.txt',$msgErr.PHP_EOL, FILE_APPEND);
 		}
 	}
