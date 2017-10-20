543b12415c2b6a3d9174d496a5cedf1a9e8c4e73
Asus Home <bla@bla.com>
2017-10-20 21:47:00 +0300

    PHP STORM - 20/10/2017 UPDATE PAGINI WEB

---
+++ web_resources/web_pages/pagina02About.php
@@ -0,0 +1,26 @@
+<div class="text-left" style="padding:50px 0" xmlns="http://www.w3.org/1999/html">
+    <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SayHi este o aplicatie de chat online care a fost realizata pentru disciplina P.I.E. (<strong>P</strong>roiect<strong> I</strong>nformatic in <strong>E</strong>chipa)
+        in cadrul <a href="http://www.ugal.ro/">Universitatea „Dunărea de Jos”</a> , Facultatea: <a href="http://www.aciee.ugal.ro/">A.C.I.E.E.</a> , Profil: <a href="http://www.aciee.ugal.ro/">C.T.I.</a> Sub
+        supravegherea Ș.l. ing. <a href="mailto:Dan.Munteanu@ugal.ro ">Dan MUNTEANU</a>.
+    </p>
+    <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Proiectul a fost realizat prin ajutorul a 2 mari componente partea organizatorica ( folosirea platformei <a href="http://www.asana.com/">asana</a> ) si normal partea
+        de cod. <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pe partea de cod sau folosit urmatoarele sub-componente: HTML , CSS , JavaScript , PHP , SQL .
+    </p>
+</div>
+
+<div class="text-left" style="padding:50px 0">
+    <div class="text-center" style="padding:1px 0">
+        <strong><p>Informati de contact:<br></strong>
+            Paladuta Stefan  <a href="mailto:stefan.paladuta17@gmail.com">stefan.paladuta17@gmail.com</a>.
+        </p>
+    </div>
+    <p><strong>Departamentul Calculatoare și Tehnologia Informației</strong><br>
+        Galaţi, str. Ştiinţei, nr. 2<br>
+        Corp G, etaj 3.<br>
+        Cod poştal: 800146<br>
+        TEL/FAX: 0236/460182<br>
+    </p>
+</div>
