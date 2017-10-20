097abcfe1be0da74a8b1d000c5327ef59cd897c1
Asus Home <bla@bla.com>
2017-10-20 06:56:17 +0300

    PHP STORM - 20/10/2017 Crearea celor 5 Pagini REGISTER FORM FORGOT PASS ABOUT CONTACT

--- web_resources/web_pages/BackPagina05.php
+++ web_resources/web_pages/BackPagina05.php
@@ -3,69 +3,39 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Proiect PIE, CTI ANUL 4">
     <meta name="author" content="Paladuta Stefan">
+    <meta name="description" content="Proiect PIE, CTI ANUL 4">
     <link rel="icon" href="../web_pictures/tabIcon.ico">
 
-    <title>Cover Template for Bootstrap</title>
     <title>Bootstrap testing PIE</title>
     <!-- Bootstrap -->
     <link href="../lib_frameworkuri/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
+    <!-- Custom styles -->
     <link href="../lib_frameworkuri/Bootstrap/cover.css" rel="stylesheet">
     <link href="../lib_frameworkuri/FontAwesome/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="site-wrapper">
-
     <div class="site-wrapper-inner">
-
-        <div class="cover-container">
-
-            <div class="masthead clearfix">
-                <div class="inner">
-                    <h3 class="masthead-brand">SayHi</h3>
-                    <nav class="nav nav-masthead">
-                        <a class="nav-link active" href="#">Home</a>
-                        <a class="nav-link" href="#">About</a>
-                        <a class="nav-link" href="#">Contact</a>
-                    </nav>
-                </div>
-            </div>
-
+        <div class="cover-container" style="">
+            <!--  include navigation bar -->
+            <?php include("NavBar.php"); ?>
             <div class="inner cover">
-
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-
-                            <!--  include form -->
+                            <!--  include about section -->
                             <?php include("pagina05ForgotPass.php"); ?>
-
                         </div>
                     </div>
                 </div>
-
             </div>
-
-            <div class="mastfoot">
-                <div class="inner">
-                    <p> <a href="https://edu.csed.ugal.ro/">Proiect CTI Anul 4 Materie: P.I.E.</a>,
-                        by <a href="https://www.facebook.com/stefan.kanames">@Paladuta Stefan</a>,
-                        <a href="https://www.facebook.com/alexandru.bot.7">@Bot Alexandru</a>,<br>
-                        <a href="https://www.facebook.com/nedelescu.cosmin">@Nedelescu Cosmin</a>,
-                        <a href="https://www.facebook.com/elektroboom5">@Oancea Iulian</a>,
-                        <a href="https://www.facebook.com/magnificul">@Petroiu Marius</a>
-                        .</p>
-                </div>
-            </div>
-
+            <!--  include footer bar -->
+            <?php include("footerBar.php"); ?>
         </div>
-
     </div>
-
 </div>
-<!-- Bootstrap core JavaScript
+<!-- Bootstrap core JavaScript .min.js && .js
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="../lib_frameworkuri/Bootstrap/js/bootstrap.js"></script>
