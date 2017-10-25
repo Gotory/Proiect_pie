fabe6af26c6e9c87ab1b8a45f72a1f31ff73e094
Asus Home <bla@bla.com>
2017-10-25 06:48:51 +0300

    pagina 01 Login AVANS IMENS !!!!!

--- BE_development/PHP/FlowDecisions/pagina01Login.php
+++ BE_development/PHP/FlowDecisions/pagina01Login.php
@@ -2,34 +2,23 @@
 include_once('../../../web_resources/SPL_MODULE_STEFAN.php');
 spl_autoload_register('my_autoloader');
 
-#Pas  scoaterea datelor din form
-
-IF(isset( $_REQUEST['tmp_username'])){
-    $userName     = $_REQUEST['tmp_username'];
-}ELSE{
-    throw new Exception("Lipseste user name");
-}
-IF(isset( $_REQUEST['tmp_remember'])){
-    $rememberUser = $_REQUEST['tmp_remember'];
-}
-IF(isset($_REQUEST['tmp_password'])){
-    $userPassIn   = $_REQUEST['tmp_password'];
-}ELSE{
-    throw new Exception("Lipseste user password");
-}
-
-
 #Pas  hash the password la inregistare !!!!
 //$userPassInput = password_hash($userPassIn,PASSWORD_DEFAULT);
 
 
-
-
     try{
-    #Pas  check if account does exist with this EMAIL or NICKNAME
-    handlerDB::checkEmail($userName);
-    #Pas  check if password incorrect
-    handlerDB::getUserWithEmailAndPass($userName,$userPassIn);
+        #Pas  scoaterea datelor din form
+        IF(isset( $_REQUEST['tmp_username'])){ $userName       = $_REQUEST['tmp_username'];}ELSE{throw new Exception("Lipseste user name");}
+        IF(isset( $_REQUEST['tmp_remember'])){ $rememberUser   = $_REQUEST['tmp_remember'];}
+        IF(isset( $_REQUEST['tmp_password'])){ $userPassIn     = $_REQUEST['tmp_password'];}ELSE{throw new Exception("Lipseste user password");}
+
+
+
+        #Pas  check if account does exist with this EMAIL or NICKNAME
+        handlerDB::checkEmail($userName);
+        #Pas  check if password incorrect
+        handlerDB::getUserWithEmailAndPass($userName,$userPassIn);
+
         header("Location: ../../../web_resources/web_pages/BackPagina06.php");
     }catch(Exception $exceptione){
         echo "Code: ".$exceptione->getCode()."<br>";
--- web_resources/web_pages/BackPagina01.php
+++ web_resources/web_pages/BackPagina01.php
@@ -19,7 +19,7 @@
         <meta name="author" content="Paladuta Stefan">
     	<meta name="description" content="Proiect PIE, CTI ANUL 4">
    		<link rel="icon" href="../web_pictures/tabIcon.ico">
-        <title>SayHi Forgot password</title>
+        <title>SayHi Login</title>
     	<!-- Bootstrap -->
     	<link href="../lib_frameworkuri/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- Custom styles -->
