4470c2ca74909a8785d2c734d0f450834f5d71d5
Asus Home <bla@bla.com>
2017-11-02 22:16:16 +0200

    STANDARDIZARE LOG

--- BE_development/PHP/DBConnectorClasses/Conexiune.php
+++ BE_development/PHP/DBConnectorClasses/Conexiune.php
@@ -6,21 +6,21 @@ private static $conn;
 public function __construct(){
 	try {
 		$log4Debug =Log4DebugFactory::getLog4DebugObject();
-		$log4Debug->debug_String("Intrat in constructor Conexiune");
-        $log4Debug->debug_StringValue("Connection pid: ",getmypid());
+		$log4Debug->debug_String("-> Intrat in constructor Conexiune <-");
+        $log4Debug->debug_StringValue("^^Conexiune^^ pid: ",getmypid());
 		$ini_array= parse_ini_file(self::returneazaCaleCatreFisierIni());
 		if ($ini_array) {
-			$log4Debug->alert_String('Loaded configInfoDB.ini: ');
+			$log4Debug->alert_String('^^Conexiune^^ Loaded configInfoDB.ini: ');
 		} else {
-			$log4Debug->alert_String('configInfoDB.ini file is not loaded');
+			$log4Debug->alert_String('^^Conexiune^^ configInfoDB.ini file is not loaded');
 		}
 		$log4Debug->debug_AssArray($ini_array);
 		if (gethostname() == 'DESKTOP-CGDSSVT') {
 			$db_hostV = 'localhost';                                  // hostname stefan
-			$log4Debug->debug_StringValue('$db_hostV: ',$db_hostV);
+			$log4Debug->debug_StringValue('^^Conexiune^^ -> $db_hostV: ',$db_hostV);
 		} else {
 			$db_hostV = $ini_array[DbIP].":".$ini_array[DbPort];      // hostname Alex,Cosmin,Marius,Iulian
-			$log4Debug->debug_StringValue('$db_hostV: ',$db_hostV);
+			$log4Debug->debug_StringValue('^^Conexiune^^ -> $db_hostV: ',$db_hostV);
 		}
 		$db_host      = $db_hostV;                    // hostname
 		$db_name      = $ini_array[DbSchemaName];     // databasename
@@ -30,7 +30,7 @@ public function __construct(){
         $con = new PDO('mysql:host='.$db_host.'; dbname='.$db_name, $db_user, $db_user_pw);
         $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         $con->exec("SET CHARACTER SET utf8");  //  return all sql requests as UTF-8
-        $log4Debug->debug_String("conexiune creata cu succese");
+        $log4Debug->debug_String("^^Conexiune^^ -> conexiune creata cu succes");
         self::setConn($con);
 	}
 	catch (PDOException $err) {
@@ -42,8 +42,7 @@ public function __construct(){
 		# file_put_contents('PDOErrors.txt',$err, FILE_APPEND);  // write some details to an error-log outside public_html - old way
 		die();  //  terminate connection
 	}
-	$log4Debug->debug_String("Iesit din constructor Conexiune");
-
+	$log4Debug->debug_String("-> Iesit din constructor Conexiune <-");
 
 }
     public function returneazaCaleCatreFisierIni(){ #-Creare cale catre fisierul de configuratie
@@ -63,23 +62,20 @@ public function __construct(){
 	public static function getInstance(){
         #start
         $log4Debug =Log4DebugFactory::getLog4DebugObject();
-        $log4Debug->debug_StringValue("getLog4DebugObject(): ",$instance);
         static $instance = null;
         if(null==$instance){
             $instance = (new Conexiune())->getConn();
-            $log4Debug->debug_String("#!!#A new Connection object created");
+            $log4Debug->debug_String("^^Conexiune^^ -> A new Connection object created");
             $log4Debug->debug_AssArray($instance->query('SELECT CONNECTION_ID()')->fetch(PDO::FETCH_ASSOC));
             //echo $instance->getAttribute(PDO::ATTR_CONNECTION_STATUS)."<br>";
         }else{
             #using same obj
-            $log4Debug->debug_String("#!!#Using same connection object");
+            $log4Debug->debug_String("^^Conexiune^^ -> Using same connection object");
             $log4Debug->debug_AssArray($instance->query('SELECT CONNECTION_ID()')->fetch(PDO::FETCH_ASSOC));
         }
         #end
         #codul de mai sus foloseste principiul la singleton. Fiecare obiect conn db chemat va fi acelasi astfel incat sa evitam crearea mereu a unui obiect nou!
         return $instance;
     }
-
-
 
 }
